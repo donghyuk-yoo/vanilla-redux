f2e35a7b0c730b6d09253c58759b8e10248432f0
YOODONGHYUK <ydh8485@gmail.com>
2021-01-29 23:24:48 +0900

    Action

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Learning Vanilla-Redux and React-Redux
 21/01/12 initial setting
 21/01/12 vanilla Counter
 21/01/13 Store and Reducer
+21/01/29 Actions
 ```
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import { createStore } from 'redux';
 
-const increase = document.getElementById("increase");
-const decrease = document.getElementById("decrease");
+const increase = document.getElementById("add");
+const decrease = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // initialize count
-const countModifier = (count = 0) => {
-  return count;
-}
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  switch (action.type) {
+    case 'ADD':
+      return count+1;
+    case 'minus':
+      return count-1;
+    default:
+      return count;
+  }
+};
 
-const store = createStore(reducer);
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'ADD'});
+countStore.dispatch({ type: 'ADD'});
+countStore.dispatch({ type: 'ADD'});
+countStore.dispatch({ type: 'ADD'});
+countStore.dispatch({ type: 'ADD'});
+countStore.dispatch({ type: 'MINUS'});
+
+console.log(countStore.getState());
