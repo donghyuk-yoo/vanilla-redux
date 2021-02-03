c9ebc0afc707740a8827c33dfca5af06971c2c29
YOODONGHYUK <ydh8485@gmail.com>
2021-02-03 22:39:47 +0900

    Recap, Refactor

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // initialize count
 const countModifier = (count = 0, action) => {
   switch (action.type) {
-    case 'ADD':
+    case "ADD":
       return count+1;
-    case 'MINUS':
+    case "MINUS":
       return count-1;
     default:
       return count;
@@ -21,19 +26,22 @@ const countStore = createStore(countModifier);
 const onChange = () => {
   number.innerText = countStore.getState();
 }
-
+  
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD"});
+  countStore.dispatch({ type: ADD});
   console.log(countStore.getState());  
 }
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS"});
+  countStore.dispatch({ type: MINUS});
   console.log(countStore.getState());
 }
 
 
 add.addEventListener("click", handleAdd); 
-minus.addEventListener("click", handleMinus); 
+minus.addEventListener("click", handleMinus);
+
+
+
