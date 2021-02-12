72f57798016281105555daa913a014af8e1f827e
YOODONGHYUK <ydh8485@gmail.com>
2021-02-13 07:26:46 +0900

    mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-
-function Home() {
+import { connect } from "react-redux";
+// connect에서 받은 props(toDos)를 인자로
+function Home({toDos}) {
   const [text,setText] = useState("");
 
   const onChange = (e) => {
@@ -18,10 +19,19 @@ function Home() {
       <input type="text" value={text} onChange={onChange}/>
       <button type="submit">submit</button>
     </form>
-    <ul></ul>
+    <ul>
+      {JSON.stringify(toDos)}
+    </ul>
     </>
   );  
 }
+// state: store의 현재값, ownProps: react-router에 의해 HOME에 주어진 props
+function mapStateToProps(state, ownProps) {
+  // 리턴한 것을 HOME에 props로 전달
+  return { toDos: state };
 
-export default Home;
+}
+// 함수(여기선 mapStateToProps)와 컴포넌트(여기선 HOME)를 이용해 react-redux의 connect를 이용
+// mapStateToProps 함수에 store 연결, redux state로부터 컴포넌트(여기선 HOME)에 prop으로 전달
+export default connect(mapStateToProps)(Home) ;
 
